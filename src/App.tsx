4a52810c2e05cd5f991439c5eebccbcd0c2572a6
Renan Brayner <renanbrayner@gmail.com>
2024-10-16 18:14:08 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { invoke } from "@tauri-apps/api/core";
+import "./App.css";
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  return (
+    <main>
+      <h1 className="text-red-500 font-bold text-3xl">Hello World!</h1>
+    </main>
+  );
+}
+
+export default App;
